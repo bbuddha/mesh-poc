c7c3931185f2032ed2bd6bf7124a9853092c3f58
Bobby Buddha <Bobby.Buddha@charter.com>
2022-02-18 12:58:06 -0600

    Updates

--- spring-petclinic-config-server/src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
+++ spring-petclinic-config-server/src/main/java/org/springframework/samples/petclinic/config/ConfigServerApplication.java
@@ -17,6 +17,7 @@ package org.springframework.samples.petclinic.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -24,6 +25,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
