5ebf1828197e6738e0f11f0ad28dcd474f8a2bbf
P2956560 <C-James.Cakalic@charter.com>
2022-03-31 14:40:04 -0500

    Added second instance of vets-service with different dataset.

---
+++ spring-petclinic-vets-service/src/main/resources/db/hsqldb/data2.sql
@@ -0,0 +1,16 @@
+INSERT INTO vets VALUES (1, 'James', 'Herriot');
+INSERT INTO vets VALUES (2, 'Alfred', 'Wight');
+INSERT INTO vets VALUES (3, 'Donald', 'Sinclair');
+INSERT INTO vets VALUES (4, 'Brian', 'Sinclair');
+INSERT INTO vets VALUES (5, 'Jane', 'Hinton');
+INSERT INTO vets VALUES (6, 'Lila', 'Miller');
+
+INSERT INTO specialties VALUES (1, 'radiology');
+INSERT INTO specialties VALUES (2, 'surgery');
+INSERT INTO specialties VALUES (3, 'dentistry');
+
+INSERT INTO vet_specialties VALUES (2, 1);
+INSERT INTO vet_specialties VALUES (3, 2);
+INSERT INTO vet_specialties VALUES (3, 3);
+INSERT INTO vet_specialties VALUES (4, 2);
+INSERT INTO vet_specialties VALUES (5, 1);
