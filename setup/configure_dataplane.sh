4e50f622ee2eaa94c1dc767f555f7455e490655e
Bobby Buddha <Bobby.Buddha@charter.com>
2022-04-07 12:10:33 -0500

    Changes : 1) Extracted token creation into a seperate service to make tokens avaiable for data plane proxies. 2) Extracted kumactl configuration into a seperate script to make it shareable. 3) Minor renaming changes.

--- setup/configure_dataplane.sh
+++ setup/configure_dataplane.sh
@@ -28,8 +28,8 @@ create_dataplane() {
   #
   # Create token for "${dataplane}"
   #
-  echo "generating dataplane token to '${dataplane}'/token"
-  kumactl generate dataplane-token --name="${dataplane}" > /"${dataplane}"/token
+  #echo "generating dataplane token to '${dataplane}'/token"
+  #kumactl generate dataplane-token --name="${dataplane}" > /"${dataplane}"/token
 
   #
   # Resolve IP address allocated to the "${dataplane}" container
@@ -70,25 +70,10 @@ create_mesh_gateway() {
   kumactl generate dataplane-token --name="${hostname}" > /"${hostname}"/token
 }
 
-cp_name=petclinic-cp
-cp_url=https://petclinic-cp:5682
-
 #
-# Configure kuma-cp
+# Configure kumactl
 #
-if [ ! -f /certs/client/cert.key ]; then
-  echo "Generating client certificate"
-  kumactl generate tls-certificate --type=client --hostname=${cp_name} --key-file=/certs/client/cert.key --cert-file=/certs/client/cert.pem
-  chmod 666 /certs/server/cert.key
-  chmod 666 /certs/server/cert.pem
-fi
-if [ ! -f /certs/server/cert.key ]; then
-  echo "Generating server certificate"
-  kumactl generate tls-certificate --type=server --cp-hostname=${cp_name} --key-file=/certs/server/cert.key --cert-file=/certs/server/cert.pem
-  chmod 666 /certs/server/cert.key
-  chmod 666 /certs/server/cert.pem
-fi
-kumactl config control-planes add --name universal --address ${cp_url} --ca-cert-file /certs/server/cert.pem --client-cert-file /certs/client/cert.pem --client-key-file /certs/client/cert.key --overwrite
+/petclinic/configure_kumactl.sh
 
 #
 # Create kuma-dp for customers service
